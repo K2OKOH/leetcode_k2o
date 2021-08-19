6cae3379b1a952b2c94fa4f28ebb91ab6f612f81
K2O <xmjtf@outlook.com>
2021-08-19 10:12:25 +0800

    create a leetcode project

---
+++ try/try.cpp
@@ -0,0 +1,15 @@
+# include<iostream>
+# include<vector>
+
+using namespace std;
+
+int main()
+{
+    int a = 1;
+    vector<int> aa;
+    aa.push_back(a);
+    a++;
+
+    cout << "hello world\n"; 
+    return 0;
+}
