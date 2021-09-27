9ecc7c2aaeb61abc57ef223f0d6550fc7670672c
K2O <xmjtf@outlook.com>
2021-09-27 22:32:25 +0800

    70 ans

---
+++ 70.Climbing_Stairs/Climbing_Stairs.py
@@ -0,0 +1,14 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if(n <= 2): 
+            return n
+        dp = [1]*(n+1)
+        for i in range(2,n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+        return dp[n]
+
+if __name__ == '__main__':
+    n = 3
+    So = Solution()
+    ans = So.climbStairs(n)
+    print(ans)
