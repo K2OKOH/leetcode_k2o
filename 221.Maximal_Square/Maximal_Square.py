05ab2c227be07e239b1a6bb236db21c3cbfe466d
K2O <xmjtf@outlook.com>
2021-10-04 09:56:30 +0800

    221 ans

--- 221.Maximal_Square/Maximal_Square.py
+++ 221.Maximal_Square/Maximal_Square.py
@@ -3,7 +3,17 @@ from typing import List
 class Solution:
     def maximalSquare(self, matrix: List[List[str]]) -> int:
         ans = 0
-        return ans
+        H = len(matrix)
+        W = len(matrix[0])
+        if(H==0 or W==0):
+            return 0
+        dp = [[0]*W for i in range(H)]
+        for idx_y in range(H):
+            for idx_x in range(W):
+                if(matrix[idx_y][idx_x] != '0'):
+                    dp[idx_y][idx_x] = min(dp[idx_y-1][idx_x-1], min(dp[idx_y-1][idx_x],dp[idx_y][idx_x-1])) + 1
+                ans = max(ans, dp[idx_y][idx_x])
+        return ans*ans
 
 
 if __name__ == '__main__':
