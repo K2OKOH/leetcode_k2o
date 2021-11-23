83901d50654984b49a4a1d7412f8e7ae80023fd2
K2O <xmjtf@outlook.com>
2021-11-23 15:17:20 +0800

    188 ans

--- 188.BestTimetoBuyandSellStockIV/BestTimetoBuyandSellStockIV.py
+++ 188.BestTimetoBuyandSellStockIV/BestTimetoBuyandSellStockIV.py
@@ -2,13 +2,27 @@ from typing import List
 import sys
 
 class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        sell = 0
-
-        return sell
+    def maxProfit(self, k: int, prices: List[int]) -> int:
+        day = len(prices)
+        dp_buy = [-sys.maxsize] * (k+1)
+        dp_sell = [0] * (k+1)
+        if(day < 2):
+            return 0
+        if (k >= day):
+            sum = 0
+            for i in range(1,k):
+                if(prices[i-1]<prices[i]):
+                    sum += prices[i] - prices[i-1]
+            return sum
+        for i in range(day):
+            for j in range(1,k+1):
+                dp_buy[j] = max(dp_buy[j], dp_sell[j-1] - prices[i])
+                dp_sell[j] = max(dp_sell[j], dp_buy[j] + prices[i])
+        return dp_sell[k]
 
 if __name__ == '__main__':
-    prices = [7,1,5,3,6,4]
+    prices = [3,2,6,5,0,3]
+    k = 2
     So = Solution()
-    ans = So.maxProfit(prices)
+    ans = So.maxProfit(k, prices)
     print(ans)
