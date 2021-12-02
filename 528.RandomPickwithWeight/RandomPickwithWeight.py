69c31440f9c3ec2e852e90735591e6aed416f1d0
K2O <xmjtf@outlook.com>
2021-12-02 10:45:42 +0800

    528 ans

---
+++ 528.RandomPickwithWeight/RandomPickwithWeight.py
@@ -0,0 +1,21 @@
+from typing import List
+import random
+import bisect
+import itertools
+class Solution:
+
+    def __init__(self, w: List[int]):
+        self.pre = list(itertools.accumulate(w))
+        self.sum = sum(w)
+
+    def pickIndex(self) -> int:
+        x = random.randint(1, self.sum)
+        return bisect.bisect_left(self.pre, x)
+
+if __name__ == '__main__':
+    nums = [[[1, 2, 3]], [], [], []]
+    actions = ["Solution","pickIndex"]
+    So = Solution(nums[0][0])
+    ans = So.pickIndex()
+    print(ans)
+
