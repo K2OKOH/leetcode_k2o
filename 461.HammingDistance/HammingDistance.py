13937e041e162a4bc6b56fb788be937a6d483ffd
K2O <xmjtf@outlook.com>
2021-12-04 22:30:02 +0800

    461 ans

---
+++ 461.HammingDistance/HammingDistance.py
@@ -0,0 +1,14 @@
+
+        
+from typing import List
+
+class Solution:
+    def hammingDistance(self, x: int, y: int) -> int:
+        return bin(x ^ y).count('1')
+
+if __name__ == '__main__':
+    x = 1
+    y = 4
+    So = Solution()
+    ans = So.hammingDistance(x, y)
+    print(ans)
