37b08fbfb55b3979f6724a2f3903a8c2a3d0e5eb
K2O <xmjtf@outlook.com>
2021-12-21 16:09:40 +0800

    23 ans

--- 23.MergekSortedLists/MergekSortedLists.py
+++ 23.MergekSortedLists/MergekSortedLists.py
@@ -1,4 +1,7 @@
 # Definition for singly-linked list.
+from heapq import heappop, heappush
+from typing import List
+
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
@@ -44,27 +47,45 @@ class SingleLinkList(object):
             current_Node.next = node
             self.length += 1
 
-# class Solution:
-#     def mergeKLists(self, lists: List[ListNode]) -> ListNode:
+class Solution:
+    def mergeKLists(self, lists: List[ListNode]) -> ListNode:
+        import heapq
+        # 无输入
+        if (len(lists) == 0):
+            return None
+        dummy = ListNode(0)
+        p = dummy
+        head = []
+        for i in range(len(lists)):
+            if (lists[i]):
+                heapq.heappush(head, (lists[i].val, i))
+                lists[i] = lists[i].next
+        while head:
+            val, idx = heapq.heappop(head)
+            p.next = ListNode(val)
+            p = p.next
+            if lists[idx]:
+                heapq.heappush(head, (lists[idx].val, idx))
+                lists[idx] = lists[idx].next
+        return dummy.next
 
-#         return List
 
 if __name__ == '__main__':
     # 初始化数组
     Input_List = [[1,4,5],[1,3,4],[2,6]]
     list_len = len(Input_List)
-    all_list = []
+    temperatures = []
     for idx in range(list_len):
         meta_list = SingleLinkList(Input_List[idx])
-        all_list.append(meta_list)
-    # So = Solution()
-    # ans = So.dailyTemperatures(temperatures)
-    # print(ans)
-    a = all_list[0]
-    for idx in range(list_len):
-        a = all_list[idx]
-        print('------')
-        while(a != None):
-            print(a.val)
-            a = a.next
-        print('------')
+        temperatures.append(meta_list)
+
+    
+    So = Solution()
+    ans = So.mergeKLists(temperatures)
+    print(ans)
+
+    print('------')
+    while(ans != None):
+        print(ans.val)
+        ans = ans.next
+    print('------')
