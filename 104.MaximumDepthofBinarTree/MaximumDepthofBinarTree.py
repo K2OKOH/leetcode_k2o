07301567eb161e2043705b21d48cbe6f1045a94e
K2O <xmjtf@outlook.com>
2022-01-21 17:54:30 +0800

    104 ans

--- 104.MaximumDepthofBinarTree/MaximumDepthofBinarTree.py
+++ 104.MaximumDepthofBinarTree/MaximumDepthofBinarTree.py
@@ -1,6 +1,6 @@
 from logging.config import valid_ident
-import queue
-from turtle import left
+import math
+import sys
 from typing import Optional
 
 class TreeNode:
@@ -15,11 +15,39 @@ class BinaryTree(object):
         self.root_node = root_node
         if (l != []):
             self.length = len(l)
+            # 是完全二叉树 length = 2^deepth - 1
+            self.deepth = math.ceil(math.log2(self.length + 1))
             for idx in range(self.length):
                 # 创建树中结点
                 n_node = TreeNode(val = l[idx])
-                self.add(n_node)
-    
+                # self.add(n_node)
+                # 无根结点，成为根结点
+                if (self.root_node == None):
+                    self.root_node = n_node
+                    continue
+                node_queue = [self.root_node]
+                while (len(node_queue) != 0):
+                    cur = node_queue.pop()
+                    if (cur.left == None):
+                        cur.left = n_node
+                    elif (cur.right == None):
+                        cur.right = n_node
+                    else:
+                        # 如果 cur 不是空结点
+                        if (n_node.val != None):
+                            node_queue.append(cur.left)
+                            node_queue.append(cur.right)
+            # 删除空结点
+            node_queue = [self.root_node]
+            while (len(node_queue) != 0):
+                cur = node_queue.pop()
+                if  (cur != None):
+                    if (cur.val == None):
+                        cur = None
+                    else:
+                        node_queue.append(cur.left)
+                        node_queue.append(cur.right)
+
     def add(self, node:TreeNode):
         # 无根结点，成为根结点
         if (self.root_node == None):
@@ -27,14 +55,27 @@ class BinaryTree(object):
         node_queue = [self.root_node]
         while (len(node_queue) != 0):
             cur = node_queue.pop()
-            if (cur.left_node == None):
-                cur.left_node = node
-            elif (cur.right_node == None):
-                cur.right_node = node
+            if (cur.left == None):
+                cur.left = node
+            elif (cur.right == None):
+                cur.right = node
             else:
-                node_queue.append(cur.left_node)
-                node_queue.append(cur.right_node)
-
+                node_queue.append(cur.left)
+                node_queue.append(cur.right)
+    
+    def DrawTree(self):
+        node_queue = [self.root_node]
+        cnt = 1
+        while (len(node_queue) != 0):
+            cur = node_queue.pop()
+            cnt += 1
+            if (cur != None):
+                sys.stdout.write(str(cur.val)+',')
+                node_queue.append(cur.left)
+                node_queue.append(cur.right)
+            if ((math.log2(cnt))%1 == 0):
+                sys.stdout.write("\n")
+                sys.stdout.flush()
 
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
@@ -47,8 +88,9 @@ if __name__ == '__main__':
     # 初始化数组
     tree_list = [3,9,20,None,None,15,7]
     root = BinaryTree(tree_list)
+    root.DrawTree()
 
     So = Solution()
-    ans = So.maxDepth(root)
+    ans = So.maxDepth(root.root_node)
     print(ans)
 
