cddac841262ebeac77f7d2c55981880e0d1aa22a
K2O <xmjtf@outlook.com>
2022-02-20 21:37:14 +0800

    208 ans

--- 208.ImplementTrie(PrefixTree)/ImplementTrie(PrefixTree).py
+++ 208.ImplementTrie(PrefixTree)/ImplementTrie(PrefixTree).py
@@ -11,10 +11,11 @@ class Trie:
         # 逐个字符向下查找
         for ch in word:
             ch = ord(ch) - ord("a")
-            # 如果找不到，返回None
+            # 如果找不到，成为新的结点
             if not node.children[ch]:
                 node.children[ch] = Trie()
             node = node.children[ch]
+        # 词结束时，标记node状态为「结束」
         node.isEnd = True
 
     def search_prefix(self, prefix: str) -> "Trie":
@@ -24,11 +25,14 @@ class Trie:
             # 查找不到返回None
             if not node.children[ch]:
                 return None
+            # 找的到的话 -> node 变成 子node
             node = node.children[ch]
+        # 返回查找到的最后一个node
         return node
 
     def search(self, word: str) -> bool:
         node = self.search_prefix(word)
+        # 如果能检测到，并且该单词已经个结束
         return node is not None and node.isEnd
 
     def startsWith(self, prefix: str) -> bool:
@@ -37,9 +41,9 @@ class Trie:
 if __name__ == '__main__':
     # 初始化数组
     trie = Trie()
-    trie.insert("apple")
+    trie.insert('apple')
     trie.search('apple')
     trie.search('app')
     trie.startsWith('app')
-    trie.insert("app")
+    trie.insert('app')
     trie.search('app')
